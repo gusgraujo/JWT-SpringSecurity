7f1feeda22b3814e88ff2f06e82ed6e540efeb61
ggaraujo@ext.worten.pt <ggaraujo@ext.worten.pt>
2023-06-13 16:51:16 +0100

    Implement filter

---
+++ src/main/java/com/project/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.project.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.lang.NonNull;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+            ) throws ServletException, IOException {
+        // TODO document why this method is empty
+    }
+}
